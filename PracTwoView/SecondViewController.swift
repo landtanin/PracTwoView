7569abd973f58b90c4671a14a73eccbc9a3e4b91
Tanin Rojanapiansatith <sp_land@hotmail.com>
2015-09-01 22:37:36 +0700

    New file for the second viewController, File in menu bar --> new --> file --> Cocoa class --> set type - UIViewController, set language - swift

---
+++ PracTwoView/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  PracTwoView
+//
+//  Created by landtanin on 9/1/2558 BE.
+//  Copyright (c) 2558 landtanin. All rights reserved.
+//
+
+import Cocoa
+
+class SecondViewController: UIViewController {
+
+}
---
+++ PracTwoView/ViewController2.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController2.swift
+//  PracTwoView
+//
+//  Created by landtanin on 9/1/2558 BE.
+//  Copyright (c) 2558 landtanin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController2: UIViewController {
+
+}
